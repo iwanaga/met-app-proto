49cfdc145ad372a49f96f4a7c6d68d9c51766900
iwanaga <iwanaga@iij.ad.jp>
2015-07-23 00:48:14 +0900

    気圧データの表示書式指定

--- client/app/pressure/pressure.controller.js
+++ client/app/pressure/pressure.controller.js
@@ -2,7 +2,52 @@
 
 angular.module('metAppApp')
   .controller('PressureCtrl', function ($scope, pressureService) {
-    var pressures = pressureService.query(function () {
-      $scope.data = pressures;
-    })
+    pressureService.query(function (data) {
+      $scope.data = [
+        {
+          key: '気圧',
+          values: data,
+          color: '#2196f3'
+        }
+      ];
+    });
+
+    $scope.options = {
+      chart: {
+        type: 'lineChart',
+        height: 400,
+        margin: {
+          top: 20,
+          right: 20,
+          bottom: 40,
+          left: 55
+        },
+        x: function(d) { return Date.parse(d.createdAt); },
+        y: function(d) { return d.pressure; },
+        useInteractiveGuideline: false,
+        dispatch: {},
+        xAxis: {
+          axisLabel: '時刻',
+          tickFormat: function (d) {
+            return d3.time.format('%H:%M')(new Date(d))
+          },
+          showMaxMin: false
+        },
+        yAxis: {
+          axisLabel: '気圧',
+          axisLabelDistance: 30,
+          showMaxMin: false
+        },
+        forceY: [null, null],
+        transitionDuration: 250,
+        callback: angular.noop
+      },
+      title: {
+        enable: true,
+        text: '気圧'
+      },
+      caption: {
+        enable: false
+      }
+    };
   });
